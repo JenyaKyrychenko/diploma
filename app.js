30e79565c40fe130ff6aac0fe798b4bb76075efd
JenyaKyrychenko <happyshi333@gmail.com>
2021-07-18 13:44:51 +0300

    predeploy settings

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const config = require('config')
+const path = require('path')
 const app = express()
 
-app.use(express.static('./public'))
+//app.use(express.static('./public'))
 
 app.use(express.json({extended:true}))
 
@@ -13,6 +14,14 @@ app.use('/api/declaration', require('./routes/declaration.routes'))
 app.use('/api/exam', require('./routes/exam.routes'))
 app.use('/api/examresults', require('./routes/examresult.routes'))
 app.use('/api/mentor', require('./routes/mentor.routes'))
+
+if(process.env.NODE_ENV === 'production'){
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = config.get('port') || 5000
 
